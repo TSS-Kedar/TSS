d81e9c64f83b9dc249764bb607b3f5875e08eccd
rvp0001 <rhishikesh.parkhi@gmail.com>
2022-10-06 09:37:31 +0530

    warp weft addition

--- src/TSS/Admin/Product/common/Weavingtype.tsx
+++ src/TSS/Admin/Product/common/Weavingtype.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { SelectInput } from '../../../../common/InputFields/Select'
-const typeoption = [{ 'key': 'Warp', 'value': 'Warp' },{ 'key': 'Weft', 'value': 'Weft' }]
+const typeoption = [{ 'key': 'Warp', 'value': 'Warp' },{ 'key': 'Weft', 'value': 'Weft' },{ 'key': 'Both', 'value': 'Both' }]
 function Weavingtype({currdoc,modifydoc,wd}:any) {
   return (
     <SelectInput wd={wd} label="Type" options={typeoption} name="type" currdoc={currdoc} section={"type"} modifydoc={modifydoc} />
